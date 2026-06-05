b4f236651c4c0fc0b20900a932905673fe3bb822
martinez-martina <117871145+martinezm02@users.noreply.github.com>
2026-06-05 20:07:52 -0300

    Cambios en ProcessingQueue
    Se pone una condición de salida a los consumidores mediante el bool despacho_terminado

--- ProcessingQueue.cpp
+++ ProcessingQueue.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int consumos = 0;
-extern bool sistema_activo;
+//extern bool sistema_activo;
 extern std::mutex coutMutex;
 
 long long espera_prioridad_0 = 0;
@@ -15,35 +15,21 @@ int cantidad_prioridad_1 = 0;
 std::mutex mtx_metricas;
 
  void consumir_paquete(ProcessingQueue &cola){
-    while(sistema_activo){
+    while(true){
     //ADQUISICION DEL MUTEX
     std::unique_lock<std::mutex> lock(cola.acceso_cola);
 
-    while(cola.cinta_transportadora.empty() && sistema_activo){
-        cola.cv_consumidores.wait(lock);
+    while (cola.cinta_transportadora.empty() && !cola.despacho_terminado) {
+            cola.cv_consumidores.wait(lock);
+     }
+
+    if (cola.cinta_transportadora.empty() && cola.despacho_terminado) {
+        return;
     }
-
-    if (cola.cinta_transportadora.empty() && !sistema_activo) {
-    return;
-}
-    //TOMAR PAQUETE DESPACHADO
-    //PaqueteEnProcesamiento paquete = cola.cinta_transportadora.front();
-
     //PROCESAMIENTO DEL PAQUETE
     auto ahora = std::chrono::steady_clock::now();
     auto tiempo_pasado = std::chrono::duration_cast<std::chrono::milliseconds>(ahora - cola.cinta_transportadora.front().hora_ingreso);
     auto tiempo_minimo = std::chrono::milliseconds(550);
-
-    //Si no pasaron 550ms el hilo duerme
-    /*while (tiempo_pasado < tiempo_minimo) {
-        // wait_for duerme el hilo, pero SUELTA EL CANDADO para que los productores sigan metiendo cajas
-        cola.cv_consumidores.wait_for(lock, tiempo_minimo - tiempo_pasado);
-
-        // Cuando el hilo se despierta, miramos la hora de nuevo a ver si ya pasaron los 550ms
-        ahora = std::chrono::steady_clock::now();
-        //tiempo_pasado = std::chrono::duration_cast<std::chrono::milliseconds>(ahora - paquete.hora_ingreso);
-        tiempo_pasado = std::chrono::duration_cast<std::chrono::milliseconds>(ahora - cola.cinta_transportadora.front().hora_ingreso);
-    }*/
 
     if (tiempo_pasado < tiempo_minimo) {
         cola.cv_consumidores.wait_for(lock, tiempo_minimo - tiempo_pasado);
